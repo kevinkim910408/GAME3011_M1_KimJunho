8eda5943be520bd8f55995577ca65bb28830239d
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-02-24 15:00:56 -0500

    Initial commit

--- Assets/Scripts/GameToggle.cs
+++ Assets/Scripts/GameToggle.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class GameToggle : MonoBehaviour
 {
-    [SerializeField] 
-    private GameObject canvas;
+    [SerializeField] GameObject modePanel;
+    [SerializeField] GameObject gamePanel;
 
     private bool active = false;
+
+    private void Start()
+    {
+        modePanel.SetActive(false);
+        gamePanel.SetActive(false);
+    }
 
     public void ToggleGame()
     {
         active = !active;
-        canvas.SetActive(active);
+        modePanel.SetActive(active);
+    }
+
+    public void StartGame()
+    {
+        modePanel.SetActive(false);
+        gamePanel.SetActive(true);
     }
 }
