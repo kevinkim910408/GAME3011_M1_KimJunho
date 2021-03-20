4b5234064794a0336898ea75fdf3c18530c08dce
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-03-20 19:08:07 -0400

    nothing

--- Assets/Scripts/Match03/GridManager.cs
+++ Assets/Scripts/Match03/GridManager.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [Header("Grid")]
-    [SerializeField] int gridRow = 0;
-    [SerializeField] int gridColonm = 0;
+    [SerializeField] int gridRow;
+    [SerializeField] int gridColonm;
 
     // 2d grid
     private MatchTiles[,] grid;
 
     [Header("Tile")]
     [SerializeField] GameObject tilePrefab = null;
-    [SerializeField] int tileWidth = 20;
-    [SerializeField] int tileHeight = 20;
+    [SerializeField] int tileWidth;
+    [SerializeField] int tileHeight;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // generate grid
         GenerateGrid();
     }
 
@@ -31,16 +33,24 @@ public class GridManager : MonoBehaviour
 
     public void GenerateGrid()
     {
+        // set grid
         grid = new MatchTiles[gridRow, gridColonm];
 
+        // place tiles
         for(int r = 0; r < gridRow; ++r)
         {
             for(int c = 0; c < gridColonm; ++c)
             {
                 GameObject tiles = Instantiate(tilePrefab, transform);
+
                 grid[r, c] = tiles.GetComponent<MatchTiles>();
+                
+                // give grid indices
                 grid[r, c].SetGridIndices(r, c);
-                grid[r, c].GetComponent<RectTransform>().localPosition = new Vector3(r * tileHeight, c * tileWidth, 0);
+
+                // tiles positions
+                grid[r, c].GetComponent<RectTransform>().localPosition 
+                    = new Vector3(r * tileHeight, c * tileWidth, 0);
             }
         }
     }
